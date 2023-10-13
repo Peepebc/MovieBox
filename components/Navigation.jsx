8f504f9a6ce976b897c27639d29eb635d7d6a777
Peepebc <jbujcab03@iesmarquesdecomares.org>
2023-10-13 18:37:42 +0200

    dwa

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,4 +1,4 @@
-import logo from '../../public/resources/moviebox.png';
+import logo from '../public/resources/moviebox.png';
 import Link from "next/link";
 import Image from 'next/image';
 import { useContext, useEffect, useState } from "react";
